4755bbf97562e59ca2e0ddcb26ae49d1853df2f7
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-07-07 17:57:34 +0100

    Add event

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,7 +1,13 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log(e);
+    }
+
     return ( 
         <div className="home">
             <h1>home page </h1>
+            <button type="button" onClick={handleClick}>Click me</button>
         </div>
      );
 }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return ( 
     <nav className="navbar">
-        <h1> The Dojo</h1>
+        <h1> The Dojo Blog</h1>
         <div className="links">
             <a href="/">Home</a>
             <a href="/Create">New Blog</a>
