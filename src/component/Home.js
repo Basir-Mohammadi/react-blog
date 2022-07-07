f83d254d3affa852ba43ae877c58766bf43de099
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-07-07 22:06:21 +0100

    Use props

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlog] = useState([
         { title: "my new website", body: "hey buddy..", author: "Ali", id: 1},
@@ -8,34 +9,9 @@ const Home = () => {
 
     return ( 
         <div className="home">
-           {blogs.map((blog) => (
-            <div className="blog-preview" key={blog.id}>
-                <h2>{blog.title}</h2>
-                <p>Written by {blog.author}</p>
-            </div>
-            ))}
+           <BlogList blogs={blogs} title = "All Blogs!"/>
         </div>
      );
 }
  
 export default Home;
-
-// import React from "react";
-// class Cat extends React.Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = {sleeping: true};
-//     }
-//     render() {
-//       return (
-//         <div>
-//           <p>{`The cat is ${this.state.sleeping ? "sleeping" : "eating"}`}</p>
-//           <button onClick={() => this.setState({ sleeping: false })}>
-//             Wake up!
-//           </button>
-//         </div>
-//       );
-//     }
-//   }
-
-//   export default Cat;
