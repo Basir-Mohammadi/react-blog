a0165030b8213c437d69876530c2b7f5212c1e92
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-07-07 18:53:28 +0100

    Add useState

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,15 +1,40 @@
+import { useState } from "react";
 const Home = () => {
-
-    const handleClick = (e) => {
-        console.log(e);
+    // let name='ali';
+    const [name, setName] = useState('Ali');
+    const [age, setAge] = useState(25);
+    const handleClick = () => {
+        setName('Ahmad');
+        setAge(21);
     }
 
     return ( 
         <div className="home">
             <h1>home page </h1>
+            <p>{name} is {age} years old</p>
             <button type="button" onClick={handleClick}>Click me</button>
         </div>
      );
 }
  
 export default Home;
+
+// import React from "react";
+// class Cat extends React.Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {sleeping: true};
+//     }
+//     render() {
+//       return (
+//         <div>
+//           <p>{`The cat is ${this.state.sleeping ? "sleeping" : "eating"}`}</p>
+//           <button onClick={() => this.setState({ sleeping: false })}>
+//             Wake up!
+//           </button>
+//         </div>
+//       );
+//     }
+//   }
+
+//   export default Cat;
--- src/component/operate.js
+++ src/component/operate.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar";
-import Home from "./Home";
+// import Home from "./Home";
+import Cat from "./Home"
 function Operate () {
     return (
         <div className="App">
             <Navbar />
             <div className="content">
-            <Home />
+            {/* <Home /> */}
+            <Cat />
             </div>
         </div>
     )
