2e898b21330566b08e65fd566dd91ea6b562bd26
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-07-07 22:12:45 +0100

    Use filter method

--- src/component/Home.js
+++ src/component/Home.js
@@ -10,6 +10,7 @@ const Home = () => {
     return ( 
         <div className="home">
            <BlogList blogs={blogs} title = "All Blogs!"/>
+           <BlogList blogs={blogs.filter((blog) => blog.author ==='Ahmad')} title = "Ahmad Blog!"/>
         </div>
      );
 }
