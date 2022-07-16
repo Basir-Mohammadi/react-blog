b5d80266601cb2af89e344e0aeb4410c3e42586d
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-07-16 21:08:55 +0100

    Use Hook

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,6 +2,8 @@ import { useState,useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlog] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !==id);
@@ -9,17 +11,30 @@ const Home = () => {
     }
 
     useEffect (()=> {
-        fetch('http://localhost:8000/blogs')
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
             .then(res => {
-                return res.json();
+                if(!res.ok) {
+                    throw Error('could not fetch data from resource');
+                }
+                    return res.json();
             })
             .then(data => {
                 setBlog(data);
+                setIsPending(false);
+                setError(null);
             })
+            .catch(err => {
+                setIsPending(false);
+                setError(err.message);
+            })
+        },1000)
     }, []);
 
     return ( 
         <div className="home">
+            { error && <div> {error } </div>}
+            {isPending && <div>Loading......</div>}
            {blogs && <BlogList blogs={blogs} title = "All Blogs!" handleDelete={handleDelete}/>}
         </div>
      );
